76e56be7baf7d99e41005b68b1487e165a7cd831
Bart House <bhouse@ntdev.microsoft.com>
2017-11-23 02:37:50 -0800

    Visual Studio build uses SDL OpenGL headers.

--- graphics/openGL/glInclude.h
+++ graphics/openGL/glInclude.h
@@ -1,20 +1,27 @@
-#ifdef __mac__
+#if defined(__mac__)
 
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
+
+#elif defined(_MSC_BUILD)
+
+// Visual Studio Build uses SDL openGL headers
+
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
 
 #else
 
 #include <GL/gl.h>
 #include <GL/glu.h>
 
-
-
-#ifdef WIN_32
+#if defined(WIN_32)
 // on Windows, some stuff that's normally in gl.h (1.2 and 1.3 stuff) is
 // in glext
 #include <GL/glext.h>
 #endif
-
 
 #endif
